d2a791639bb5170ea441cfd4ecc8e88fbeee9861
Fajar <fajarstok15@gmail.com>
2025-10-31 00:36:54 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+BASE_DIR     = 'Data'
+DUMP_DIR     = 'Dump'
+LOGIN_DIR    = 'Login'
+RESULT_DIR   = 'Result'
+CHECK_DIR    = 'Checkpoint'
+USER_DATA    = BASE_DIR + '/UserLogin.json'
+
+BASE_URL_APP = 'https://i.instagram.com'
+BASE_URL_WEB = 'https://www.instagram.com'
---
+++ run.py
@@ -0,0 +1,5 @@
+from main import Terminal, InstagramMenu
+
+if __name__ == "__main__":
+	Terminal().check_folder()
+	InstagramMenu().menu()
